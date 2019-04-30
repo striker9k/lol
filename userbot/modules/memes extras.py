f1bcac08e526ce83ddaae04018deebd98b495a27
Thago <47388359+Thagoo@users.noreply.github.com>
2019-04-30 18:23:16 +0530

    some changes

--- userbot/modules/memes extras.py
+++ userbot/modules/memes extras.py
@@ -181,7 +181,7 @@ async def _(event):
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🖤💜💙❤💚💛🧡🖤"))
+	deq = deque(list("🧡❤🖤💜💙💚💛❤🧡"))
 	for _ in range(32):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
