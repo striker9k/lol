805b33f58c2b3cc5feeebfae4a09a82f43188e14
Ayush Raj <36340649+Ayushraj21@users.noreply.github.com>
2019-05-23 08:31:33 +0530

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -14,7 +14,7 @@ from distutils.util import strtobool as sb
 from dotenv import load_dotenv
 from requests import get
 from telethon import TelegramClient
-
+from telethon.sessions import StringSession
 
 load_dotenv("config.env")
 
@@ -49,6 +49,8 @@ if CONFIG_CHECK:
     quit(1)
 
 API_KEY = os.environ.get("API_KEY", None)
+
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
 API_HASH = os.environ.get("API_HASH", None)
 
@@ -88,7 +90,7 @@ SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
 DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
 # pylint: disable=invalid-name
-bot = TelegramClient("userbot", API_KEY, API_HASH)
+bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
 
 if os.path.exists("learning-data-root.check"):
     os.remove("learning-data-root.check")
